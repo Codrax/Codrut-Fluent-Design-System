02b298ee2c58dfec3cf929e99b72ec0aed28ecab
Codrut <petculescucodrut@gmail.com>
2023-01-31 18:02:31 +0200

    Added project files
    The units and project files have been added

---
+++ FluentUXDemo.dpr
@@ -0,0 +1,14 @@
+program FluentUXDemo;
+
+uses
+  Vcl.Forms,
+  CFXTest in 'Demo\CFXTest.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Source/CFX.Classes.pas
@@ -0,0 +1,13 @@
+unit CFX.Classes;
+
+interface
+  uses
+    Vcl.Graphics;
+
+  type
+    FXPImageKind = (ikFontIcon, ikImage);
+    FXControlState = (csNone, csHover, csPress);
+
+implementation
+
+end.
